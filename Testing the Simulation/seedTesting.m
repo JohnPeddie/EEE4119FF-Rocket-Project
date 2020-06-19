8e2cd290e4c772b1338891a9368a2cb74224e997
john <johnpeddie118@gmail.com>
2020-06-19 13:04:52 +0200

    88% sucess rate

--- Testing the Simulation/seedTesting.m
+++ Testing the Simulation/seedTesting.m
@@ -1,4 +1,4 @@
-totalRuns = 20;
+totalRuns = 100;
 define_constants;
 seeds = randi(10000,totalRuns,1);
 q = reshape(seeds',1,[]);
@@ -6,7 +6,7 @@ successes = 0;
 for i = q
     set_param(['AsteroidImpact', '/rngSeed'], 'Value', int2str(i));
     sim('AsteroidImpact');  % tell simulink to simulate the model
-    Scenario = 2;
+    Scenario = 1;
     successes = successes + (mission_complete(x, y, ast_x, ast_y, ast_th, Scenario));
     
 
